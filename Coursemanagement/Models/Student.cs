739330efd92c27d4915f191a27c23dd1104998f3
alibabashovv <90691151+alibabashovv@users.noreply.github.com>
2021-10-18 21:00:34 +0400

    Add files via upload

---
+++ Coursemanagement/Models/Student.cs
@@ -0,0 +1,33 @@
+﻿using Coursemanagement.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coursemanagement.Models
+{
+    class Student
+    {
+        
+        public string Fullname { get; set; }
+        public int Groupno { get; set; }
+        public Educationtype Edutype { get; set; }
+
+
+        public Student(string fullname, int groupno, Educationtype edutype)
+        {
+            Fullname = fullname;
+            Groupno = groupno;
+            Edutype = edutype;
+        }
+        public override string ToString()
+        {
+
+            return $"{Fullname}   {Edutype}  {Groupno} ";
+
+        }
+
+    }
+}
+
+       
+     
---
+++ Coursemanagement/Program.cs
@@ -0,0 +1,60 @@
+﻿using Coursemanagement.Service;
+using System;
+
+namespace Coursemanagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to course application");
+
+            int selection;
+            do
+            {
+                Console.WriteLine("1.Creategroup");
+                Console.WriteLine("2.ListofGroups");
+                Console.WriteLine("3.Editgroups");
+                Console.WriteLine("4.Groupstudents");
+                Console.WriteLine("5.Allstudents");
+                Console.WriteLine("6.Createstudent");
+                Console.WriteLine("  ");
+                Console.WriteLine("Please select one of these");
+
+                string n = Console.ReadLine();
+                bool result = int.TryParse(n, out selection);
+
+                if (result)
+                {
+                    switch (selection)
+                    {
+                        case 1:
+                            Menuservice.Creategroup();
+                            break;
+                        case 2:
+                            Menuservice.Listofgroups();
+                            break;
+                        case 3:
+                            Menuservice.Editgroupmenu();
+                            break;
+                        case 4:
+                            Menuservice.Groupstudents();
+                            break;
+                        case 5:
+                            Menuservice.Allstudents();
+                            break;
+                        case 6:
+                            Menuservice.Createstudent();
+                            break;
+                            
+                           
+                            
+
+                        default:
+                            break;
+                    }
+                }
+            } while(selection !=0);
+        }
+    }
+}
