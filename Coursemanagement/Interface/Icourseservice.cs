53b2470d057a06862e23601e12da0fd0ac4b87da
alibabashovv <alimb@code.edu.az>
2021-10-18 01:50:15 -0700

    Add project files.

---
+++ Coursemanagement/Interface/Icourseservice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Coursemanagement.Enums;
+using Coursemanagement.Models;
+
+namespace Coursemanagement.Interface
+{
+    interface Icourseservice
+    {
+        public List<Group> Groups { get;}
+
+        string Creategroup(int numberofstudent, int groupnumber, Professioncategory category);
+
+        void Listofgroups();
+
+        void Editgroups(string number, string newnumber);
+
+        void Groupstudents(string stu);
+
+        void Allstudents();
+
+        string Createstudent(string fullname,int age, string Istype,int groupno);
+        
+    }
+}
---
+++ Coursemanagement/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coursemanagement.Models
+{
+    class Student
+    {
+        public static int acount = 0;
+        public string Fullname { get; set; }
+        public int Groupno { get; set; }
+        public string IsType { get; set; }
+
+
+        public Student(string fullname, int groupno, string itstype)
+        {
+            Fullname = fullname;
+            Groupno = groupno;
+            IsType = itstype;
+        }
+
+        
+    }
+}
+
+       
+     
